ee282b48eb6cba7f92b320b8c157bde45c4ebdde
netcom_cbj\netcom_bjcho <bjcho@netcomsolution.co.kr>
2024-03-11 09:48:44 +0900

    headline modify

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,7 +4,7 @@ export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">
-        Netcom_Zoom_test
+        Netcom_Zoom
       </h1>
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         These are test pages for using {" "}
@@ -16,7 +16,6 @@ export function Intro() {
         </a>{" "}
         and {CMS_NAME}.
       </h4>
-      <script data-apikey="aERpcc4gSZSOM_JcHsn0ng" data-env="us01" src="https://us01ccistatic.zoom.us/us01cci/web-sdk/zcc-sdk.js"></script>
     </section>
   );
 }
