a247d0120141b805b3899cd8e889f4c41c5f0731
netcom_cbj\netcom_bjcho <bjcho@netcomsolution.co.kr>
2024-06-18 19:43:43 +0900

    0618_1 image M, morestroy D

--- src/app/_components/cover-image.tsx
+++ src/app/_components/cover-image.tsx
@@ -16,7 +16,7 @@ const CoverImage = ({ title, src, slug }: Props) => {
       className={cn("shadow-sm w-full", {
         "hover:shadow-lg transition-shadow duration-200": slug,
       })}
-      width={1300}
+      width={1800}
       height={630}
     />
   );
