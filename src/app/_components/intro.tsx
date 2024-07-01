847dd5ec74b6a0236755808ca85baedd8ffed90c
netcom_cbj\netcom_bjcho <bjcho@netcomsolution.co.kr>
2024-07-01 15:08:21 +0900

    intro modified - copyright del

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,13 +3,15 @@ import Link from "next/link";
 
 export function Intro() {
   return (
-    <section className="flex-col md:flex-row flex items-center md:justify-between mt-16 mb-16 md:mb-12">
+    <section className="flex-col md:flex-row flex items-right md:justify-between mt-16 mb-16 md:mb-12">
       <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">        
           Netcom Solution
       </h1>
+      /*
       <h4 className="text-center md:text-left text-lg mt-5 md:pl-8">
         Beyond The Technology for Customer
       </h4>
+      */
     </section>
   );
 }
