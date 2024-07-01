d53a2f273e69c6af39d47b8060402b1f9f93f22b
netcom_cbj\netcom_bjcho <bjcho@netcomsolution.co.kr>
2024-07-01 16:25:52 +0900

    werwer

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -5,11 +5,11 @@ export function Intro() {
   return (
     <section className="flex-col flex items-right md:justify-between mt-8 mb-8 md:mt-4 mb-4">
       
-        <h1 className="text-4xl md:text-8xl font-bold tracking-tighter leading-tight">        
+        <h1 className="text-4xl md:text-4xl font-bold tracking-tighter leading-tight">        
             Netcom Solution
         </h1>
         
-        <div id="btn-wrapper" className="text-3xl md:text-8xl font-bold tracking-tighter leading-tight text-right">
+        <div id="btn-wrapper" className="text-3xl md:text-4xl font-bold tracking-tighter">
           ☰
           <div id="chat-buuton">
           </div>
