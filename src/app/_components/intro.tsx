f2d7073e2591380b7e48ecdd3351af77e30385f9
netcom_cbj\netcom_bjcho <bjcho@netcomsolution.co.kr>
2024-07-01 16:36:08 +0900

    wewerwer

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 
 export function Intro() {
   return (
+<<<<<<< HEAD
     <section className="flex-col flex items-right md:justify-between mt-8 mb-8 md:mt-4 mb-4">
       
         <h1 className="text-6xl md:text-6xl font-bold tracking-tighter leading-tight pr-8 md: pr-8">        
@@ -10,11 +11,20 @@ export function Intro() {
         </h1>
         
         <div id="btn-wrapper" className="text-6xl md:text-6xl font-bold tracking-tighter mb-8 text-right md: mb-8">
+=======
+    <section className="flex-col md:flex-row flex items-right md:justify-between mt-16 mb-16 md:mb-12">
+      <div id="oneline">
+        <h1 className="text-3xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">        
+            Netcom Solution
+        </h1>
+        
+        <div id="btn-wrapper" className="text-2xl md:text-6xl font-bold text-right md:text-center">
+>>>>>>> parent of de4701f (jhjhj)
           ☰
           <div id="chat-buuton">
           </div>
         </div>
-
+      </div>
     
     </section>
   );
