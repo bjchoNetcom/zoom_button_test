da05ab3801d8c7f361270791768ed31cb61654e5
netcom_cbj\netcom_bjcho <bjcho@netcomsolution.co.kr>
2024-07-01 15:32:09 +0900

    footer button del, intro button add

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,10 +4,15 @@ import Link from "next/link";
 export function Intro() {
   return (
     <section className="flex-col md:flex-row flex items-right md:justify-between mt-16 mb-16 md:mb-12">
-      <h1 className="text-5xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">        
+      <h1 className="text-3xl md:text-8xl font-bold tracking-tighter leading-tight md:pr-8">        
           Netcom Solution
       </h1>
       
+      <div id="btn-wrapper" className="text-left md:test-left">
+        ☰
+        <div id="chat-buuton">
+        </div>
+      </div>
     
     </section>
   );
