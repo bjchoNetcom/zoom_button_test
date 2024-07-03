562750634651e7f6246d69a95e6a0671dcac1d6f
netcom_cbj\netcom_bjcho <bjcho@netcomsolution.co.kr>
2024-07-03 11:23:26 +0900

    sdsd

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -5,7 +5,7 @@ import Image from "next/image";
 export function Intro() {
   return (
 
-    <section className="fixed top-0 left-0 w-full flex items-end justify-between p-2 bg-white shadow-md z-20">
+    <section className="fixed top-0 left-0 w-full flex items-end justify-between p-2 m-auto bg-white shadow-md z-20">
 
       <div className="flex items-center">
         <h2 className="text-6xl md:text-6xl font-bold tracking-tighter leading-tight pr-8 md:pr-8">
