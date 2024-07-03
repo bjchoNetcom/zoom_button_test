88ddcf93fb3f53653f9ff2b3081da20dae4f8c72
netcom_cbj\netcom_bjcho <bjcho@netcomsolution.co.kr>
2024-07-03 10:44:34 +0900

    intro M

--- src/app/_components/intro.tsx
+++ src/app/_components/intro.tsx
@@ -4,20 +4,18 @@ import Link from "next/link";
 export function Intro() {
   return (
 
-    <section className="fixed top-0 left-0 w-full flex items-center justify-between p-4 bg-white shadow-md z-50">
+    <section className="fixed top-0 left-0 w-full flex items-end justify-between p-4 bg-white shadow-md z-20">
 
       <div className="flex items-center">
         <img 
-            src="/public/logo.png" 
+            src="@/public/logo.png" 
             alt="Netcom Solution Logo" 
-            width={200} // 로고 이미지의 너비
-            height={100} // 로고 이미지의 높이
             className="object-contain"
           />
       
 
         <div id="btn-wrapper">
-          <span className="text-2xl">☰</span>
+          <span className="text-4xl">☰</span>
         </div>
       </div>
     </section>
